0e0f0f4640ac02476ec0afe801955c4bdda6a9fd
rajanraj2 <rajanraj253545@gmail.com>
2025-03-31 12:15:43 +0530

    comparison btw data types

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log("2" > 1);
+// console.log("2" < 1);
+
+console.log(null > 0)
+console.log(null == 0);
+console.log(null >= 0); // true
+
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+
+// === also compares the data type
